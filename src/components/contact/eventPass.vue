0a1cda403b207307a8455b2559b358ec703211d1
mayyingzi <3205009763@qq.com>
2018-12-10 17:22:22 +0800

    add some message with component contact

--- src/components/contact/eventPass.vue
+++ src/components/contact/eventPass.vue
@@ -1,5 +1,7 @@
 <template>
     <div class="eventPass">
+        <p>任意组件之间通信，也可以使用eventBus实例扩展，为其添加 data， methods，computed等属性，作为公共属性，可以共用；所以在这里可以放置需要共享的信息，比如用户登录昵称等，使用只需要初始化一次bus既可以，所以在单页面客户端应用较广泛。</p>
+        <p>项目较大，可以使用vuex状态管理</p>
         <van-button type="warning" @click="sendMessage">发送消息</van-button>
     </div>
 </template>
@@ -27,5 +29,6 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
-
+p
+    text-align left
 </style>
