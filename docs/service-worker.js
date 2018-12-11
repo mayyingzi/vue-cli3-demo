98c734d3eedd934951f5e3ca6aa9f797db35d82a
mayyingzi <3205009763@qq.com>
2018-12-11 14:30:00 +0800

    change path

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.57c555345c5406aeb56f0a57dfda9ea4.js"
+  "/precache-manifest.c1d82e3789894a038d691862c56fe345.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "vue-js-demo"});
--- vue.config.js
+++ vue.config.js
@@ -17,7 +17,7 @@ class MyAwesomeWebpackPlugin {
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 
 module.exports = {
-    baseUrl: './', // 部署包时的基本url， 也可以根据环境判断设置 process.env.NODE_ENV === 'production'? '/production-sub-path/' : '/'
+    baseUrl: '/', // 部署包时的基本url， 也可以根据环境判断设置 process.env.NODE_ENV === 'production'? '/production-sub-path/' : '/'
     outputDir: 'docs', // 当运行vue -cli-service build 时生成的生产换进构建文件的目录， 默认 dist
     assetsDir: 'static', // 防止生成的静态资源（js、css、img、fonts）的（相对于outputDir）目录
     indexPath: 'index.html', // 指定生成的index.html的输出路径（相对于outputDir）。也是一个绝对路径，默认：index.html
